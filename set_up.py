11cc197892b7056a42f1798b76a5eb29d625d108
Grzegorz Salacinski <salacinski.grzegorz@gmail.com>
2019-06-28 00:41:24 +0200

    Add set_up file to automate configuration, update readme

---
+++ set_up.py
@@ -0,0 +1,46 @@
+import os
+from getpass import getpass
+
+
+def check_password(password):
+    invalid_chars = "~`#$&*()\\|[]{};'<>/?!"
+    new_pass = ""
+    for i in password:
+        if i in invalid_chars:
+            new_pass += f"\\{i}"
+        else:
+            new_pass += i
+    return new_pass
+
+
+def set_up():
+    workspace = input("Select name for your workspace folder: ")
+    username = input("Type your github username: ")
+    password = getpass(prompt="Type your github password: ")
+    home = os.environ["HOME"]
+    export_lines = [
+        "export WORKSPACE=$HOME/{}".format(workspace),
+        'export GITHUB_USER="{}"'.format(username),
+        'export GITHUB_PASS="{}"'.format(check_password(password)),
+        "source {}/.my_command.sh".format(os.path.abspath(os.path.dirname(__file__))),
+    ]
+
+    if not os.path.exists("{}/{}".format(home, workspace)):
+        os.makedirs(home + "/" + workspace)
+        print("Main projects folder {} created succesfully".format(workspace))
+
+    with open(f"{home}/.bashrc", "a+") as f:
+        r = open(f"{home}/.bashrc", "r")
+        content = r.read()
+        for i in export_lines:
+            if i not in content:
+                f.write(i + "\n")
+                print("{} succesfully added to file".format(i))
+            else:
+                print("Content {} already exists in requested file".format(i))
+        r.close()
+        f.close()
+
+
+if __name__ == "__main__":
+    set_up()
