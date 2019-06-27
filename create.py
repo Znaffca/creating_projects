5f1c6a9e187bb4cf17e66c9fa0aa8d1bfef98ea7
Grzegorz Salacinski <salacinski.grzegorz@gmail.com>
2019-06-27 15:59:47 +0200

    Reformat the code

--- create.py
+++ create.py
@@ -9,10 +9,12 @@ password = os.environ["GITHUB_PASS"]
 
 
 def check(char):
-    regex = re.compile('[@!#$%^&*()<>?/\\|}{~:]')
+    regex = re.compile("[@!#$%^&*()<>?/\\|}{~:]")
     if regex.search(char) is None:
         return char
-    return "Incorrect project name. Please enter the name using only alphanumeric chars."
+    return (
+        "Incorrect project name. Please enter the name using only alphanumeric chars."
+    )
 
 
 def get_github_user(username, password):
@@ -24,14 +26,14 @@ def create():
         project_name = str(sys.argv[1])
     except IndexError:
         print("Please type your project name first!")
-        
+
     if check(project_name) != project_name:
         print(check(project_name))
         exit(0)
 
     project_name = check(project_name)
     try:
-        os.makedirs(project_path + '/' +  project_name)
+        os.makedirs(project_path + "/" + project_name)
         print("Project folder generate succesfull!")
     except FileExistsError:
         print("That project already exists!")
